53ec3b7ff5e78201f56ffa74ec6b635ea27b5fc4
lllewis <lone.un@gmail.com>
2015-01-31 12:10:15 +0800

    video: update html
    Signed-off-by: lllewis <lone.un@gmail.com>

--- controllers/media.py
+++ controllers/media.py
@@ -9,4 +9,47 @@ class Test:
     def GET(self):
         web.header('Content-type','text/html')
         web.header('Transfer-Encoding','chunked')
-        return self.render.mediaServer()
+        videos = [
+                    {'title':   u'...Test...',
+                    'video': ["LoverCD1.mp4", "LoverCD2.mp4"],
+                    'nu': 2,
+                    'info': u'.CD1.',
+                    'discription': u'.CD1.',
+                    'copy_right': u'lewis'},
+
+                    {'title':   u'...H...',
+                    'video': ["h.1.mp4", "h.2.mp4", "h.3.mp4"],
+                    'nu': 3,
+                    'info': u'.CD1.',
+                    'discription': u'.CD1.',
+                    'copy_right': u'lewis'},
+
+                    {'title':   u'...AirLineStewardess...',
+                    'video': ["airlineStewardess-1.mp4", "airlineStewardess-4.mp4", "airlineStewardess-3.mp4", "airlineStewardess-4.mp4"],
+                    'nu': 4,
+                    'info': u'.CD1.',
+                    'discription': u'.CD1.',
+                    'copy_right': u'lewis'},
+
+                    {'title':   u'...喜剧之王...',
+                    'video': ["xjzw.mp4"],
+                    'nu': 1,
+                    'info': u'.CD1.',
+                    'discription': u'.CD1.',
+                    'copy_right': u'lewis'},
+
+                    {'title':   u'...我的野蛮女友...',
+                    'video': ["MyCurelGirlFriend.mp4"],
+                    'nu': 1,
+                    'info': u'.CD1.',
+                    'discription': u'.CD1.',
+                    'copy_right': u'lewis'},
+
+                    {'title':   u'...Test...',
+                    'video': ["loveLession.mp4"],
+                    'nu': 1,
+                    'info': u'.CD1.',
+                    'discription': u'.CD1.',
+                    'copy_right': u'lewis'},
+                 ]
+        return self.render.mediaServer(videos)
